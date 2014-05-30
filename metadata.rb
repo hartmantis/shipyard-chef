8d9294069df77d7213b625792be9c2cd3d69e244
Jonathan Hartman <j@p4nt5.com>
2014-05-29 21:41:20 -0700

    Initial commit; a base project generated by ChefDK

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'shipyard'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures shipyard'
+long_description 'Installs/Configures shipyard'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: shipyard
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
